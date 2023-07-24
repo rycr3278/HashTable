e2b6bbb7db9d67c39fa2416cfd898410078dd7fc
Ryan <rycr3278@colorado.edu>
2023-07-24 14:16:10 +0000

    Not much to say

--- code/Hash.h
+++ code/Hash.h
@@ -34,14 +34,14 @@ struct hash_node {
   bool deleted;
 
   // key is the key the user provides to associate with a value.
-  std::string key;
+  string key;
 
   // hashcode is the hashcode computed by the hash table's hash_func
   // on the key. it is stored to avoid having to re-compute it.
   unsigned int hashcode;
 
   // value is the user value associated with the key.
-  std::string value;
+  string value;
 };
 
 // you can define your custom types like this one
@@ -86,7 +86,7 @@ struct hash_table {
   // to use this:
   //
   // unsigned int hashcode_of_foo = mytable->hash_func("foo");
-  unsigned int (*hash_func)(std::string);
+  unsigned int (*hash_func)(string);
 
   // bucket_func is the currently in-use bucket indexing function. it
   // should be initialized to the modulo_bucket_func function. assign
@@ -94,10 +94,113 @@ struct hash_table {
   unsigned int (*bucket_func)(unsigned int hashcode, unsigned int capacity); 
 };
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+
+struct node {
+  string data;               // data: the value of this node
+  shared_ptr<node> next;  // next: pointer to the next node in the list, or NULL if this is the last node.
+};
+
+// Linked List Invariant: following the 'next' links in a linked list
+// node must eventually lead to a NULL reference signifying the end of
+// the list. (E.g. no circular references are allowed.)
+class LinkedList {
+public:
+  // constructor, initialize class variables and pointers here if need.
+  // Initially set top pointer to a null shared_ptr of node
+  LinkedList();
+
+  //deconstructor, if you are using shared_pointers you don't need to keep track of
+  // freeing memory and you can leave this blank
+  ~LinkedList();
+
+  // init_node creates a new node structure from heap memory. It points
+  // to NULL and holds the provided integer. The function returns a
+  // pointer to that new node.
+  shared_ptr<node> InitNode(string data);
+
+  // report creates a space-separated string representing the contents
+  // of the list, in the same order they are found beginning from the
+  // top of the list. return that string object.  For example, this
+  // might return "" (the empty string) if the list is empty, or it
+  // might return "1 7 3 " (note the space at the end).
+  string Report();
+
+  // append_data adds a node onto the end of the list pointed to by
+  // top. the resulting list is one element longer, and the newly
+  // appended node has the given data value. consider using the 'append'
+  // function to help.
+  void AppendData(string data);
+
+  // append is the same as append_data, except we're adding a node, rather
+  // than a value. 
+  void Append(shared_ptr<node> new_node);
+
+  // insert_data inserts a new node that contains the given data value,
+  // such that the new node occupies the offset indicated. Any nodes
+  // that were already in the list at that offset and beyond are shifted
+  // down by one. For example if the list contains 23, 74, 93, and we
+  // insert 42 at index 1, the resulting list should be 23, 42, 74, 93.
+  //
+  // If an offset is beyond the end of the list, this function should
+  // have no effect.
+  //
+  // Inserting at the end of the list should have the same effect as
+  // appending.
+  void InsertData(int offset, string data);
+
+  // insert is the same as insert_data, except we're inserting a node,
+  // rather than a value.
+  void Insert(int offset, shared_ptr<node> new_node);
+
+  // remove removes the node indicated by the given offset and frees its
+  // memory. For example if our list contains 23, 74, 93, and we remove
+  // offset 1, the resulting list should then contain 23, 93.
+  //
+  // If an invalid offset is given (e.g. negative or beyond the end of
+  // the list), this function has no effect.
+  void Remove(int offset);
+
+  // size returns the number of nodes in the linked list reachable by
+  // head before (but not including) the terminating NULL link. E.g., an
+  // empty list has size 0, a list with one item has size 1, etc.
+  int Size();
+
+  // contains returns true if the linked list pointed to by head
+  // contains a node with the specified value, false otherwise.
+  bool Contains(string data);
+
+  // This function is implemented for you
+  // Returns the top pointer
+  shared_ptr<node> GetTop();
+
+  // This function is implemented for you
+  // sets a given pointer as the top pointer
+  void SetTop(shared_ptr<node> top_ptr);
+
+  // you can add more public member variables and member functions here if you need
+
+private:
+  shared_ptr<node> top_ptr_;
+
+  // you can add add more private member variables and member functions here if you need
+
+
+
+};
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+
 // DJB2 is a hash function that hashes strings and gives an unsigned
 // integer hash code that could take on any value in the 32-bit
 // unsigned integer range. It is implemented for you.
-unsigned int DJB2(std::string key);
+unsigned int DJB2(string key);
 
 // ModuloBucketFunc simply returns hashcode % cap. It is provided
 // here as a separate function because you *could* write your own hash
@@ -120,7 +223,7 @@ public:
 
   // InitNode creates and initializes a hash_node that will occupy a
   // hash table bucket. see the hash_node documentation for more info.
-  shared_ptr<hash_node> InitNode(std::string key, unsigned int hashcode, std::string val);
+  shared_ptr<hash_node> InitNode(string key, unsigned int hashcode, string val);
 
   // SetKVP establishes a mapping between the given key and value pair
   // in the provided hash table. if the key (as identified by its hash
@@ -135,7 +238,7 @@ public:
   //
   // the return value should be true unless there was no room in the
   // hash table.
-  bool SetKVP(shared_ptr<hash_table> tbl, std::string key, std::string value);
+  bool SetKVP(shared_ptr<hash_table> tbl, string key, string value);
 
   // Load returns a load factor describing how 'full' the table
   // is. because we are using linear probing, which leaves 'deleted'
@@ -148,17 +251,17 @@ public:
   // GetVal returns the value associated with the provded key in the
   // table, or the empty string "" if no such mapping exists (or if
   // there is a mapping but it is deleted).
-  std::string GetVal(shared_ptr<hash_table> tbl, std::string key);
+  string GetVal(shared_ptr<hash_table> tbl, string key);
 
   // Contains returns true if the table has a non-deleted node whose
   // hashcode matches the given key's hashcode.
-  bool Contains(shared_ptr<hash_table> tbl, std::string key);
+  bool Contains(shared_ptr<hash_table> tbl, string key);
 
   // Remove marks as 'deleted' any existing non-deleted hash_node with
   // the same hashcode as the provided key's hashcode. if it finds such
   // a hash node, it decrements the table's size (but not occupancy) and
   // returns true. it returns false otherwise.
-  bool Remove(shared_ptr<hash_table> tbl, std::string key);
+  bool Remove(shared_ptr<hash_table> tbl, string key);
 
   // Resize creates a new underlying backing array (tbl->table) with the
   // provided new capacity, rehashes the existing backing array into the
